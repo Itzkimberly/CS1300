d70a48afc9274d158327d6985275bf1725c5c639
Itzkimberly <KERamirez01@indianatech.net>
2026-02-24 16:33:19 -0500

    CS1300 HW 3 (Part 4) Problem 1

---
+++ List_Lecture1.py
@@ -0,0 +1,6 @@
+# 1. Create a list called 'animals' with: "cat", "dog", "bird", "fish", "hamster"
+
+# Your code here: 
+# 2. Print the first animal using positive indexing.
+# 3. Print the last animal using negative indexing.
+# 4. Print how many animals are in the list.
